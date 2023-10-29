567fb6e30affe0ea92d82e181c54b1220858a4c6
LB-Squishy <laurentleboulbar@gmail.com>
2023-10-29 20:18:46 +0100

    Section Projet: Tri Projets

--- src/components/Main/Section3Projet/index.jsx
+++ src/components/Main/Section3Projet/index.jsx
@@ -1,25 +1,82 @@
 import "./style.scss";
 import ProjetCard from "./ProjetCard";
 import projets from "../../../data/projets.json";
+import { useState } from "react";
 /**
  *
  */
 
 function SectionProjet() {
     const projetsReverses = [...projets].reverse();
+    const [filter, setFilter] = useState("Tous");
+    function filterProjects(projetsReverses, filter) {
+        if (filter === "Tous") {
+            return projetsReverses;
+        } else if (filter === "Formation") {
+            return projetsReverses.filter(
+                (projet) => projet.type === "Formation"
+            );
+        } else if (filter === "Personnel") {
+            return projetsReverses.filter(
+                (projet) => projet.type === "Personnel"
+            );
+        } else if (filter === "En Cours") {
+            return projetsReverses.filter((projet) => projet.stade === "");
+        } else if (filter === "Terminé") {
+            return projetsReverses.filter(
+                (projet) => projet.stade === "Terminé"
+            );
+        }
+        return projetsReverses;
+    }
     return (
         <section id="projet">
             <div className="section-projet-container">
                 <h2 className="section-projet-container__title">Mes Projets</h2>
                 <div className="filtres-container">
-                    <div className="filtres-container__btn">Tous</div>
-                    <div className="filtres-container__btn">Formation</div>
-                    <div className="filtres-container__btn">Professionnels</div>
-                    <div className="filtres-container__btn">En Cours</div>
-                    <div className="filtres-container__btn">Terminés</div>
+                    <button
+                        className={`filtres-container__btn ${
+                            filter === "Tous" ? "active" : ""
+                        }`}
+                        onClick={() => setFilter("Tous")}
+                    >
+                        Tous
+                    </button>
+                    <button
+                        className={`filtres-container__btn ${
+                            filter === "Formation" ? "active" : ""
+                        }`}
+                        onClick={() => setFilter("Formation")}
+                    >
+                        Formation
+                    </button>
+                    <button
+                        className={`filtres-container__btn ${
+                            filter === "Personnel" ? "active" : ""
+                        }`}
+                        onClick={() => setFilter("Personnel")}
+                    >
+                        Personnel
+                    </button>
+                    <button
+                        className={`filtres-container__btn ${
+                            filter === "En Cours" ? "active" : ""
+                        }`}
+                        onClick={() => setFilter("En Cours")}
+                    >
+                        En Cours
+                    </button>
+                    <button
+                        className={`filtres-container__btn ${
+                            filter === "Terminé" ? "active" : ""
+                        }`}
+                        onClick={() => setFilter("Terminé")}
+                    >
+                        Terminé
+                    </button>
                 </div>
                 <div className="projet-container">
-                    {projetsReverses.map((projet) => (
+                    {filterProjects(projetsReverses, filter).map((projet) => (
                         <ProjetCard
                             key={`projet-${projet.id}`}
                             title={projet.title}
