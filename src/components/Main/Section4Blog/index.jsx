243d993bf18785f5a19c859e9f24a688c57c1a84
LB-Squishy <laurentleboulbar@gmail.com>
2023-10-30 15:08:10 +0100

    section blog

--- src/components/Main/Section4Blog/index.jsx
+++ src/components/Main/Section4Blog/index.jsx
@@ -8,48 +8,10 @@ function SectionBlog() {
     return (
         <section id="blog">
             <div className="section-blog-container">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum
-                asperiores sed, dolores nemo, voluptates repudiandae a deserunt
-                commodi nobis adipisci quis quaerat alias corporis sit? Facilis
-                molestiae vero est et? Lorem ipsum, dolor sit amet consectetur
-                adipisicing elit. Et minus sit delectus dolore amet architecto
-                aut, neque ipsum quas distinctio quos ad nostrum ducimus
-                pariatur, quo, dolores blanditiis dolor aliquam. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit. Dolore ducimus
-                illo libero nihil corrupti eos provident, consequuntur inventore
-                nesciunt ut. Velit expedita nam ullam nostrum, eligendi mollitia
-                optio tempore laudantium!Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Harum asperiores sed, dolores nemo, voluptates
-                repudiandae a deserunt commodi nobis adipisci quis quaerat alias
-                corporis sit? Facilis molestiae vero est et? Lorem ipsum, dolor
-                sit amet consectetur adipisicing elit. Et minus sit delectus
-                dolore amet architecto aut, neque ipsum quas distinctio quos ad
-                nostrum ducimus pariatur, quo, dolores blanditiis dolor aliquam.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
-                ducimus illo libero nihil corrupti eos provident, consequuntur
-                inventore nesciunt ut. Velit expedita nam ullam nostrum,
-                eligendi mollitia optio tempore laudantium!Lorem ipsum dolor
-                sit, amet consectetur adipisicing elit. Harum asperiores sed,
-                dolores nemo, voluptates repudiandae a deserunt commodi nobis
-                adipisci quis quaerat alias corporis sit? Facilis molestiae vero
-                est et? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Et minus sit delectus dolore amet architecto aut, neque
-                ipsum quas distinctio quos ad nostrum ducimus pariatur, quo,
-                dolores blanditiis dolor aliquam. Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit. Dolore ducimus illo libero nihil
-                corrupti eos provident, consequuntur inventore nesciunt ut.
-                Velit expedita nam ullam nostrum, eligendi mollitia optio
-                tempore laudantium!Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Harum asperiores sed, dolores nemo, voluptates
-                repudiandae a deserunt commodi nobis adipisci quis quaerat alias
-                corporis sit? Facilis molestiae vero est et? Lorem ipsum, dolor
-                sit amet consectetur adipisicing elit. Et minus sit delectus
-                dolore amet architecto aut, neque ipsum quas distinctio quos ad
-                nostrum ducimus pariatur, quo, dolores blanditiis dolor aliquam.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
-                ducimus illo libero nihil corrupti eos provident, consequuntur
-                inventore nesciunt ut. Velit expedita nam ullam nostrum,
-                eligendi mollitia optio tempore laudantium!
+                <h2 className="section-blog-container__title">Blog</h2>
+                <div className="section-blog-container__content">
+                    Coming Soon ...
+                </div>
             </div>
         </section>
     );
